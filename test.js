ec0664c16c27051ba22b955b1db6f101469c898c
mcanince <67429658+mcanince@users.noreply.github.com>
2020-06-25 18:35:26 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,6 @@
+function logIn() {
+var username=document.getElementById("Input").value;
+
+document.getElementById("message").innerHTML= username + ", Welcome to your Universe.";
+document.getElementById("message2").innerHTML="Request Accepted"
+}
