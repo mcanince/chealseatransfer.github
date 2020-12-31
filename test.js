8aa06883d7e2b0134c32cee82ab8b52d0b82823b
mcanince <67429658+mcanince@users.noreply.github.com>
2020-12-31 22:23:56 +0300

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-function logIn() {
-var username=document.getElementById("Input").value;
-
-document.getElementById("message").innerHTML= username + ", Welcome to your Universe.";
-document.getElementById("message2").innerHTML="Request Accepted"
-}
